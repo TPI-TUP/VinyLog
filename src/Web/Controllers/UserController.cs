9b4182bb7fd25ba8b8cf137fa785aaecbadd27cb
Ayrton Leis Goncebat <adleisgoncebat@gmail.com>
2026-04-23 00:17:19 -0300

    agrego controller de user

---
+++ src/Web/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace User.Web.Controllers;
+
+[Route("api/users")]
+[ApiController]
+public class UserController : ControllerBase
+{
+
+    [HttpGet("{userId}", Name = "GetUser")]
+    public ActionResult<UserDto> GetUser(int userId)
+    {
+        var user = _userService.GetUser(userId);
+
+        if (user is null)
+            return NotFound();
+
+        return Ok(user);
+
+    }
+
+    [HttpPost]
+    public ActionResult<UserDto> CreateUser(UserCreateRequest newUser)
+    {
+        var createdUser = _userService.CreateUser(newUser);
+
+        return CreatedAtRoute(
+            "GetUser",
+            new
+            {
+                userId = createdUser.Id
+            },
+            createdUser
+        );
+    }
+}
