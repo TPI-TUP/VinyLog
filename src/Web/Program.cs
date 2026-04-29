2379efa77de384e912f839754c324b1819526d63
EveRania <ev.r@live.com.ar>
2026-04-29 16:54:58 -0300

    implementa entity framework

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,4 +1,7 @@
 using Application.Services;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,19 @@ builder.Services.AddScoped<ArtistService>();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Configure the SQLite Connection
+var connection = new SqliteConnection("Data Source=VinyLogDataBase.db");
+connection.Open();
+
+// Set journal mode to DELETE using PRAGMA statement
+using (var command = connection.CreateCommand())
+{
+    command.CommandText = "PRAGMA journal_mode = DELETE;";
+    command.ExecuteNonQuery();
+}
+
+builder.Services.AddDbContext<ApplicationContext>(dbContextOptions => dbContextOptions.UseSqlite(connection));
 
 var app = builder.Build();
 
