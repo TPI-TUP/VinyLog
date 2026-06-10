14361cc17316d4a3e50c18f5bd7372475ea18f07
EveRania <ev.r@live.com.ar>
2026-06-10 09:15:04 -0300

    crea notfound y appvalidation exceptions

---
+++ src/Domain/Exceptions/AppValidationException.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class AppValidationException : Exception
+    {
+        public AppValidationException()
+        : base()
+        {
+        }
+
+        public AppValidationException(string message)
+            : base(message)
+        {
+        }
+
+        public AppValidationException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+    }
+}
