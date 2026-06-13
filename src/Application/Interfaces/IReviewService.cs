b16debad1b908befb6bcdd2067e57cff7b495517
EveRania <ev.r@live.com.ar>
2026-06-13 20:55:15 -0300

    crea IReviewService

---
+++ src/Application/Interfaces/IReviewService.cs
@@ -0,0 +1,23 @@
+using Application.Models;
+using Application.Models.Requests;
+
+namespace Application.Interfaces;
+
+public interface IReviewService
+{
+    Task<List<ReviewDto>> GetAllAsync();
+
+    Task<ReviewDto?> GetByIdAsync(int id);
+
+    Task<List<ReviewDto>> GetByAlbumAsync(int albumId);
+
+    Task<ReviewDto> AddAsync(
+        CreateReviewDto dto,
+        int userId);
+
+    Task UpdateAsync(
+        int id,
+        UpdateReviewDto dto);
+
+    Task DeleteAsync(int id);
+}
